85a7a65300af67739dde662dd52f350dcb08c89c
russell <rrlittle2@gmail.com>
2016-07-14 16:04:57 -0500

    fixed a thing

---
+++ rocket/mapping_managers/Data2CalcMappingManager.py
@@ -0,0 +1,21 @@
+from MappingManagers import MappingManagers
+# from data_handlers import wtpcalchandler
+from Functions import default_func,data_sum,mean
+
+
+class Data2CalcMM(MappingManagers):
+	"""docstring for Data2CalcMM"""
+	def __init__(self, source=sourceManager, sink = sinkManager):
+		super(Data2CalcMM, self).__init__(source=sourceManager, 
+											sink = sinkManager)
+		
+	def load_function(self):
+		functions = {}
+		functions['mean'] = mean
+		functions['data_sum'] = data_sum
+
+
+		return{};
+
+
+		
