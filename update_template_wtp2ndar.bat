811256c21c058ec6e3de38ab511f95bcb5fd1ded
xujunjiejack <xujunjiejack@gmail.com>
2017-05-10 13:32:48 -0500

    add update bat

---
+++ update_template_wtp2ndar.bat
@@ -0,0 +1,4 @@
+REM This bat starts the template
+call R:\prep_for_ruby.bat
+REM python "R:\master script\rocket data mapper and uploader\rocket" -mm coins2ndar -c
+python "Q:\Source_Code\rocket\rocket\rocket" -mm wtp2ndar -u
