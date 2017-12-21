2de8c86fe52da5a2d090805c94c30eb6015ad1c0
xujunjiejack <xujunjiejack@gmail.com>
2017-12-21 15:13:28 -0600

    Added

---
+++ rocket/qa_kit/qa_checker.py
@@ -0,0 +1,8 @@
+# This qa checker will do the qa at the end of the convert. It can also be added as an independent functionality
+# The first priority is to check the duplicate GUID in the data
+# Duplicate URSI
+
+# The second is to check whether any data exists
+
+
+# The design is probably
